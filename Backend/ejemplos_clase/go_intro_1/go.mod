068c3832ce094d7d1a7e2bfe102314f80da9cb61
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2024-04-19 10:50:21 -0500

    Add files via upload

---
+++ Backend/ejemplos_clase/go_intro_1/go.mod
@@ -0,0 +1,5 @@
+module github.com/camilocorreaUdeA/go_demo
+
+go 1.22.0
+
+require github.com/golang-module/carbon/v2 v2.3.12 // indirect
---
+++ Backend/ejemplos_clase/go_intro_1/miPaquete/utilidades.go
@@ -0,0 +1,7 @@
+package mipaquete
+
+import "fmt"
+
+func MyFunction(nombre string) string {
+	return fmt.Sprintf("Hola %s, esta es mi funcion", nombre)
+}
