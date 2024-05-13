fb526cbb56caf0699a8453db4adb9f1a42e9ed2a
camilocorreaUdeA <42076547+camilocorreaUdeA@users.noreply.github.com>
2024-05-13 15:45:26 -0500

    Update queries.sql

--- Backend/ejemplos_clase/go_docker_compose/queries.sql
+++ Backend/ejemplos_clase/go_docker_compose/queries.sql
@@ -10,4 +10,4 @@ VALUES
 ('2023-10-14 07:33:12', 'Estaban pasando bueno, no? xD', 10),
 ('2024-04-29 23:59:59', 'Felicitaciones, se ven muy bien juntos', 5),
 ('2022-12-30 12:11:45', 'Happy New Year!', 20),
-('2024-04-29 23:59:59', 'You look great!', 2) RETURNING id;
+('2024-04-29 23:59:59', 'You look great!', 2);
